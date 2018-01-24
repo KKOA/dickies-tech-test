0b84cbabff582a08f200305fe0c3b72ea13f79b3
keith(MacBookPro) <ka33145@googlemail.com>
2018-01-24 11:04:35 +0000

    Refactored & cleaned up code

--- JS Solution.js
+++ JS Solution.js
@@ -3,74 +3,50 @@ var data =
 "b": "1.00",
 "c": 1, "d": "2",
 "e": 5.1,
-"f": [1],
-"g": { "v": 1.5 }
+"f": [1,7],
+"g": { "v": 1.5 },
+"h":  0
   };
-// data.filterNonNumeric = function() {
-//   array =[]
-//     for (var i in this){
-//       console.log(i);
-//       if
 
-//       if(isNaN(data[i]))
-//       {
-//         console.log(data[i]);
-//       }
-//       console.log('')
-//     }
-//     // return this.a + " " + this.b;
-// };
-// console.log(findValues(data,'v'));
-console.log('-----')
-console.log(findNumbers(data));
-console.log('-----')
+data.filterNonNumeric = function()
+{
+	return findNumericHelper(this, []);
+};
 
-// console.log(data.filterNonNumeric());
-function findNumbers(obj){
-	return findValuesHelper(obj, []);
-}
+function findNumericHelper(obj, list)
+{
+  if (!obj && typeof obj != 'number') return list;
+  //check obj falsy and not of type number
 
-function findValuesHelper(obj, list) {
-  // console.log(typeof obj)
-  if (!obj) return list;
-  if (obj instanceof Array) {
-    for (var i in obj) {
-        list = list.concat(findValuesHelper(obj[i], []));
+  if (obj instanceof Array)
+  {
+    for (var i in obj)
+    {
+        list = list.concat(findNumericHelper(obj[i], []));
     }
     return list;
   }
-  // if (obj[key]) list.push(obj[key]);
-  // if (isNaN(obj)) list.push(obj)
-
-  else if ((typeof obj == "object") && (obj !== null) ){
+  if ((typeof obj == "object") && (obj !== null) )
+  {
 	  var children = Object.keys(obj);
-	  if (children.length > 0){
-	  	for (i = 0; i < children.length; i++ ){
-	        list = list.concat(findValuesHelper(obj[children[i]], []));
+	  if (children.length > 0)
+	  {
+	  	for (i = 0; i < children.length; i++ )
+	  	{
+	        list = list.concat(findNumericHelper(obj[children[i]], []));
 	  	}
 	  }
   }
-  else if ((typeof obj != 'string' ) && !isNaN(obj)) list.push(obj)
+  else if ((typeof obj != 'string' ) && !isNaN(obj))
+  {// check if obj is not of type string and numerical
+    list.push(obj)
+  }
   return list;
 }
-
-// function findValuesHelper(obj, key, list) {
-//   if (!obj) return list;
-//   if (obj instanceof Array) {
-//     for (var i in obj) {
-//         list = list.concat(findValuesHelper(obj[i], key, []));
-//     }
-//     return list;
-//   }
-//   if (obj[key]) list.push(obj[key]);
-
-//   if ((typeof obj == "object") && (obj !== null) ){
-// 	  var children = Object.keys(obj);
-// 	  if (children.length > 0){
-// 	  	for (i = 0; i < children.length; i++ ){
-// 	        list = list.concat(findValuesHelper(obj[children[i]], key, []));
-// 	  	}
-// 	  }
-//   }
-//   return list;
-// }
+console.log('data :')
+console.log('----------------------')
+console.log(data)
+console.log('')
+console.log('data.filterNonNumeric :')
+console.log('----------------------')
+console.log(data.filterNonNumeric());
