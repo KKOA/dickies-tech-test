6b9e61b95e5ee3a02acfd86ae820dcfb10156356
KKOA <ka33145@googlemail.com>
2019-02-06 19:22:10 +0000

    Refactored php/CsvGenerator.php

--- php/CsvGenerator.php
+++ php/CsvGenerator.php
@@ -1,45 +1,76 @@
 <?php
-/*
-Description : Class to generate csv file.
-Author : Keith Amoah
-Date : 24/01/2018
+/**
+ * Class to generate csv file.
+ * 
+ * @author Keith Amoah
+ * @copyright 24/01/2018
 */
 
 class CsvGenerator
 {
-  private $filename;
-  private $filehandle;
 
-  public function __construct($filename='') {
+    /**
+   * @var string $filename
+   */
+
+  private $filename;
+
+  private $fileHandle; //file pointer or FALSE
+
+    /**
+   * @const string EXT 
+   */
+  const EXT = '.csv';
+
+  /**
+   * On instantiate an object, opens a file pointer and points at file in write mode. If file does not exist, it creates the file.
+   * Open file in write mode.  User can specify a filename or it uses payroll[current year].csv
+   * @param string $filename
+   */
+
+  public function __construct($filename = null) {
     print "\nOpen file connection\n";
-    if($filename != '')
-    {
-     $this->$filename =$filename.'.csv';
-     echo $this->$filename;
-    }
-    else {
-     $this->$filename= $filename = 'payroll'.date("Y").'.csv';
-    }
-    // $this->filehandle = fopen($filename,'w');
-    $this->filehandle = fopen($this->$filename,'w');
+    if($filename)
+     $this->$filename = $filename;
+    else
+     $this->$filename = 'payroll'.date("Y");
+
+    $this->$filename .= self::EXT;
+    echo $this->$filename;
+
+    $this->fileHandle = fopen($this->$filename,'w');
    }
 
+
+    /**
+    * Generate array of all the headers
+    * @return array 
+    */
    private function createHeader()
    {
      return ['Month','Salary date','Bonus date'];
    }
 
+   /**
+    * Add data to the csv file
+    * @param array $data
+    */
+   
    public function createContext($data) {
         print "\nWriting to file\n";
         array_unshift($data, $this->createHeader());
         foreach ($data as $fields)
         {
-          fputcsv($this->filehandle, $fields);
+          fputcsv($this->fileHandle, $fields);
         }
     }
 
+  /**
+   * Closes an open file pointer after last call to the object
+   * 
+   */
    public function __destruct() {
     print "\nClose file connection\n";
-    fclose($this->filehandle);
+    fclose($this->fileHandle);
    }
 }
